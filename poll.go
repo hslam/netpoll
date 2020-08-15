0a39a4fa8e48be6c740ba317417688aacc8021fe
hslam <791874158@qq.com>
2020-08-16 02:59:22 +0800

    add copyright

---
+++ poll.go
@@ -0,0 +1,5 @@
+// Copyright (c) 2020 Meng Huang (mhboy@outlook.com)
+// This package is licensed under a MIT license that can be found in the LICENSE file.
+
+// Package poll supports non-blocking I/O on file descriptors with polling.
+package poll
--- poll_other.go
+++ poll_other.go
@@ -1,3 +1,6 @@
+// Copyright (c) 2020 Meng Huang (mhboy@outlook.com)
+// This package is licensed under a MIT license that can be found in the LICENSE file.
+
 // +build !linux,!darwin,!dragonfly,!freebsd,!netbsd,!openbsd
 
 package poll
